e8ff8ea0d15001eba81d84405d2eb34a150dcda5
Michael Malura <michael@malura.de>
2025-07-25 16:45:00 +0200

    Implement 'add modules' feature

---
+++ src/assets/source/modules/display.py
@@ -0,0 +1,8 @@
+# calliopemini-module: display@1.33.7
+
+from calliopemini import *
+
+# some display module code
+
+x = 10
+y = 30
---
+++ src/types/global.d.ts
@@ -0,0 +1,1 @@
+declare const MODULE_FILE_LIST: { name: string; content: string }[]; 
